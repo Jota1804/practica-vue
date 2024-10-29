a2493c90d2eaa993d177faab2b96088df76748f6
Jota1804 <jesuspichardo1804@gmail.com>
2024-10-29 18:01:06 -0400

    1:09:00

--- practica-vue/src/App.vue
+++ practica-vue/src/App.vue
@@ -1,13 +1,24 @@
 <script setup>
-const handleClick = () => {
-  console.log("diste click");
+const handleClick = (message) => {
+  // console.log("diste click");
+  console.log(message);
 };
 </script>
 
 <template>
   <h1>hello word</h1>
-  <!-- para trabajar con eventos -->
-  <button v-on:click="handleClick">Click me</button>
+  <!-- para trabajar con eventos
+  <button v-on:click="handleClick">Click me</button> -->
+  <!-- se puede abrebiar con el @ -->
+  <!-- <button @:click="handleClick('message 1')">Click me</button> -->
+
+  <!-- existen modificadores -->
+  <!-- existe el prevent default tambien -->
+  <button v-on:click.left.prevent="handleClick('message izquierdo')">
+    Click izquierdooo
+  </button>
+  <button @click.middle="handleClick('message medio')">Click medio</button>
+  <button @click.right="handleClick('message derecho')">Click derecho</button>
 </template>
 
 <style>
